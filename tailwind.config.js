665fd8042e12e9895257ebf8155995a5bb58e0cb
viral-sangani <viral.sangani2011@gmail.com>
2025-03-25 23:40:52 +0700

    feat: add GT Alpina font family and update Tailwind CSS configuration. Introduce new font styles in fonts.css and import it in index.css. Add multiple font files for various weights and styles to enhance typography.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
-        primary: '#FCFF52',
-        secondary: '#476520',
-        background: '#FCF6F1',
-        'background-secondary': '#E7E3D4',
+        primary: "#FCFF52",
+        secondary: "#476520",
+        background: "#FCF6F1",
+        "background-secondary": "#E7E3D4",
+      },
+      fontFamily: {
+        sans: ["GT Alpina", "sans-serif"],
+        serif: ["GT Alpina", "serif"],
+        mono: ["GT Alpina Typewriter", "monospace"],
       },
     },
   },
